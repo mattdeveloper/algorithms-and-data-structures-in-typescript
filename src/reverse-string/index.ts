ec08f57c12dbb224643698a3447a9d0582a149e0
Matt <talk@mattdeveloper.com>
2022-06-18 14:56:09 -0300

    add reverse string description

--- src/reverse-string/index.ts
+++ src/reverse-string/index.ts
@@ -1,3 +1,10 @@
+/**
+ * It has to take a string and return a string with the characters in reverse order.
+ *
+ * @param string
+ * @returns string
+ */
+
 function reverseString(string: string): string {
   return '';
 }
